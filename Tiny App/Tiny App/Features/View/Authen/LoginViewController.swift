085c36a4f2430e5e989958c0b25d464f79f213e5
Hockey <5906791+hockeyrun@users.noreply.github.com>
2020-12-21 19:40:44 +0700

    .update

--- Tiny App/Tiny App/Features/View/Authen/LoginViewController.swift
+++ Tiny App/Tiny App/Features/View/Authen/LoginViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
-    
+    var viewModel: SocialViewModel! {
+        didSet {
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel = SocialViewModel(viewController: self)
         // Do any additional setup after loading the view.
-        let loginButton = FBLoginButton()
-        loginButton.center = view.center
-        view.addSubview(loginButton)
+//        let loginButton = FBLoginButton()
+//        loginButton.center = view.center
+//        view.addSubview(loginButton)
     }
     
+    @IBAction func loginWithFB(_ sender: Any) {
+        viewModel.tapFBLogin()
+    }
     
     /*
      // MARK: - Navigation
